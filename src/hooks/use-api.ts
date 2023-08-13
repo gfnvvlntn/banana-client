b4e555fd6dd3ce9a4952be0526500ca179578aa2
gfnvvlntn <gfnvvlntn@gmail.com>
2023-08-13 15:20:58 +0300

    feat(app): change ulr back

--- src/hooks/use-api.ts
+++ src/hooks/use-api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const BASE_URL = `http://localhost:8080/api`
+export const BASE_URL = `http://212.60.20.101/api`
 
 const api = axios.create({
     withCredentials: true,
