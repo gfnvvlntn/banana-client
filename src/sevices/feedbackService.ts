29a254ab55cf0cea5c1b11657419bbe0462f6c8c
gfnvvlntn <gfnvvlntn@gmail.com>
2023-08-13 19:29:50 +0300

    feat(app): changed feedback

--- src/sevices/feedbackService.ts
+++ src/sevices/feedbackService.ts
@@ -2,7 +2,7 @@ import api from "../hooks/use-api";
 
 
 export default class FeedbackService {
-    static async sendFeedback(email: string, text: string) {
-        return api.post("/send-feedback", { email, text });
+    static async sendFeedback(email: string, feedback: string) {
+        return api.post("/send-feedback", { email, feedback });
     }
 }
